840dcbe23da2b9e729b068505c78510dca14b7c8
Shuvradeep Bera <shuvradeepbera2005@gmail.com>
2025-06-23 16:23:05 +0530

    base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <-- add this!
   server: {
     proxy: {
       '/socket.io': 'http://localhost:3000',
