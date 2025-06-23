caf2cb780f7684d84eeeb296313c4ad8a914c9fa
Shuvradeep Bera <shuvradeepbera2005@gmail.com>
2025-06-23 16:24:20 +0530

    socket

--- frontend/src/util/socket.js
+++ frontend/src/util/socket.js
@@ -1,2 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io("https://chess-backend-28s0.onrender.com");
+export const socket = io("https://chess-backend-28s0.onrender.com",{
+    transports: ['websocket'],
+    withCredentials: true
+});
